ebaf6aa82c442abba599843f693d25e7035bb0df
gegabayte <xasanboymuhammaddinov41@gmail.com>
2024-05-27 08:53:56 +0500

    react-beautiful at the end

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ReactBuetifull from './components/ReactBuetifull'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-
+      <div className='w-[1440px] mx-auto mt-40 shadow-lg'>
+        <div className='w-[1392 px] mx-auto bg-[#F8F8F8] rounded-lg p-16'>
+          <h1 className='text-[24px] mb-4'>Loyiha  ketma-ketligi</h1>
+          <ReactBuetifull></ReactBuetifull>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/components/ReactBuetifull.jsx
@@ -0,0 +1,78 @@
+import { FiPlusSquare } from "react-icons/fi";
+
+import { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+const ReactBuetifull = () => {
+    const [cards, setCards] = useState([
+        { id: "card-1", content: "" }
+    ]);
+    const [newCardInput, setNewCardInput] = useState("");
+
+    const onDragEnd = result => {
+        if (!result.destination) {
+            return;
+        }
+
+        const reorderedCards = Array.from(cards);
+        const [removed] = reorderedCards.splice(result.source.index, 1);
+        reorderedCards.splice(result.destination.index, 0, removed);
+
+        setCards(reorderedCards);
+    };
+
+    const handleInputChange = (event, cardId) => {
+        const updatedCards = cards.map(card =>
+            card.id === cardId ? { ...card, content: event.target.value } : card
+        );
+        setCards(updatedCards);
+    };
+
+    const handleSubmit = () => {
+        cards.forEach(card => {
+            console.log(card.content);
+        });
+    };
+
+    const addNewCard = () => {
+        const newCard = {
+            id: `card-${cards.length + 1}`,
+            content: newCardInput
+        };
+        setCards([...cards, newCard]);
+        setNewCardInput("");
+    };
+
+    return (
+        <div>
+            <DragDropContext onDragEnd={onDragEnd}>
+                <Droppable droppableId="droppable">
+                    {(provided, snapshot) => (
+                        <div {...provided.droppableProps} ref={provided.innerRef} style={{ background: snapshot.isDraggingOver ? "lightblue" : "lightgrey", padding: 25, width: 1360 }} >
+                            {cards.map((card, index) => (
+                                <Draggable key={card.id} draggableId={card.id} index={index}>
+                                    {(provided, snapshot) => (
+                                        <div className="flex justify-between gap-8"
+                                            ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} style={{ userSelect: "none", padding: 16, margin: "0 0 8px 0", background: snapshot.isDragging ? "lightgreen" : "grey", ...provided.draggableProps.style }} >
+                                            <input className="w-full h-12 p-4 rounded-lg" type="text" value={card.content} onChange={event => handleInputChange(event, card.id)} placeholder="Ustun nomi" />
+                                            <input className="w-full h-12 p-4 rounded-lg" type="number" value={card.content} onChange={event => handleInputChange(event, card.id)} placeholder="Key" />
+
+                                        </div>
+
+                                    )}
+                                </Draggable>
+                            ))}
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
+            <div className="flex justify-between mt-12">
+                <button onClick={addNewCard} className="flex items-center gap-2 text-[#5D9AFC]"><FiPlusSquare /> Ustun qo’shish</button>
+                <button onClick={handleSubmit} className="bg-[#5D9AFC] text-white w-36 h-12 rounded-lg">Submit</button>
+            </div>
+        </div>
+    );
+};
+
+export default ReactBuetifull;
